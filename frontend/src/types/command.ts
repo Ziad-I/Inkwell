59a31a0cd8c9f29467b0a72fdb57bcf7707bf7a1
Ziad <68874104+Ziad-I@users.noreply.github.com>
2025-09-02 20:02:38 +0300

    feat: add command basic interface

---
+++ frontend/src/types/command.ts
@@ -0,0 +1,11 @@
+export interface Command {
+  execute(): void;
+  destroy(): void;
+  undo(): void;
+  redo?(): void;
+  description?: string;
+
+  commit?(): void;
+  update?(): void; // Called during operation to update visual state
+  canCommit?(): boolean; // Whether the command is in a valid state to commit
+}
