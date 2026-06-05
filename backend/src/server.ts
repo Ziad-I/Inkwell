2aaa9f413ab4cf81c95e7d203e9a151c6ebce817
Ziad <68874104+Ziad-I@users.noreply.github.com>
2026-06-05 20:00:47 +0300

    feat: add server bootstrapping

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,37 @@
 import { env } from "@/config/config.js";
 import { app } from "@/app.js";
 import logger from "@/config/logger.js";
+import { createSocketServer } from "@/socket/server.js";
+import { connectRedis, disconnectRedis } from "./redis/client.js";
 
-app.listen(env.PORT, () => {
-  logger.info(`[server]: Server is running at http://localhost:${env.PORT}`);
-});
+async function startServer() {
+  try {
+    await connectRedis();
+
+    const { io, httpServer } = createSocketServer(app);
+
+    httpServer.listen(env.PORT, () => {
+      logger.info(
+        `[server]: Server is running at http://localhost:${env.PORT}`,
+      );
+    });
+
+    const gracefulShutdown = async () => {
+      logger.info("[server]: Shutting down server...");
+      io.close();
+      httpServer.close(async () => {
+        await disconnectRedis();
+        logger.info("[server]: Server shutdown complete.");
+        process.exit(0); // Exit the process with a success code
+      });
+    };
+
+    process.on("SIGINT", gracefulShutdown);
+    process.on("SIGTERM", gracefulShutdown);
+  } catch (error) {
+    logger.error(`[server]: Failed to start server - ${error}`);
+    process.exit(1); // Exit the process with an error code
+  }
+}
+
+void startServer();
