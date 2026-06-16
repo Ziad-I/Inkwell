16d496ccf95adabf7c940e874ffc6905b30285af
Ziad <68874104+Ziad-I@users.noreply.github.com>
2026-06-16 14:44:31 +0300

    fix: toast close button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ export default function App() {
           <Route path="*" element={<ErrorPage />} />
         </Routes>
       </BrowserRouter>
-      <Toaster richColors />
+      <Toaster richColors closeButton />
     </ThemeProvider>
   );
 }
