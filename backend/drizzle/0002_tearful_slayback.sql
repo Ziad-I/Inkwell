8578f6f2c82a66c59751b660409a1a3f5d500ae3
Ziad <68874104+Ziad-I@users.noreply.github.com>
2026-08-11 22:35:57 +0300

    fix: migration error due to file names

--- backend/drizzle/0002_tearful_slayback.sql
+++ backend/drizzle/0002_tearful_slayback.sql
@@ -8,6 +8,6 @@ CREATE TABLE "refresh_token" (
 	CONSTRAINT "refresh_token_token_hash_unique" UNIQUE("token_hash")
 );
 --> statement-breakpoint
-ALTER TABLE "board" ALTER COLUMN "owner_id" SET DATA TYPE uuid;--> statement-breakpoint
+ALTER TABLE "board" ALTER COLUMN "owner_id" SET DATA TYPE uuid USING owner_id::uuid;--> statement-breakpoint
 ALTER TABLE "refresh_token" ADD CONSTRAINT "refresh_token_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "board" ADD CONSTRAINT "board_owner_id_user_id_fk" FOREIGN KEY ("owner_id") REFERENCES "public"."user"("id") ON DELETE no action ON UPDATE no action;
