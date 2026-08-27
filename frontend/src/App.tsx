67b8a588ee99f4358525df902703bf233936290e
Ziad <68874104+Ziad-I@users.noreply.github.com>
2026-08-27 21:36:55 +0300

    feat: use lazy loading for board page to decrease bundle chunk size

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router";
 import HomePage from "@/pages/home";
 import ErrorPage from "@/pages/error";
-import BoardPage from "@/pages/board";
 import LoginPage from "@/pages/login";
 import RegisterPage from "@/pages/register";
 import InvitePage from "@/pages/invite";
@@ -10,30 +10,41 @@ import { ThemeProvider } from "@/providers/themeProvider";
 import { Toaster } from "@/components/ui/sonner";
 import AppLayout from "./layout";
 import { ProtectedRoute } from "@/components/auth/protectedRoute";
+import { LoadingSpinner } from "./components/home/LoadingSpinner";
+
+const BoardPage = lazy(() => import("@/pages/board"));
 
 export default function App() {
   return (
     <ThemeProvider>
       <BrowserRouter>
-        <Routes>
-          <Route element={<AppLayout />}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/invite/:token" element={<InvitePage />} />
-            <Route
-              path="/dashboard"
-              element={
-                <ProtectedRoute>
-                  <DashboardPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="*" element={<ErrorPage />} />
-          </Route>
+        <Suspense
+          fallback={
+            <div className="absolute inset-0 z-50 flex items-center justify-center">
+              <LoadingSpinner />
+            </div>
+          }
+        >
+          <Routes>
+            <Route element={<AppLayout />}>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="/invite/:token" element={<InvitePage />} />
+              <Route
+                path="/dashboard"
+                element={
+                  <ProtectedRoute>
+                    <DashboardPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="*" element={<ErrorPage />} />
+            </Route>
 
-          <Route path="/board/:roomId" element={<BoardPage />} />
-        </Routes>
+            <Route path="/board/:roomId" element={<BoardPage />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
       <Toaster richColors closeButton />
     </ThemeProvider>
